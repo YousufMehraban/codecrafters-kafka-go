fb1c730facd399d38c7774c008887c420ea84851
Mohammad Yousuf Mehraban <yousufmehraban@gmail.com>
2026-03-23 11:42:48 -0700

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -13,18 +13,43 @@ var _ = os.Exit
 
 
 
-func sendResponse(connection net.Conn) error {
+// func sendResponse(connection net.Conn) error {
 	
-	const message_size uint32 = 4
-	const correlation_id uint32 = 4
-	byteSlice := make([] byte, 8)
+// 	const message_size uint32 = 4
+// 	const correlation_id uint32 = 4
+// 	byteSlice := make([] byte, 8)
 
-	binary.BigEndian.PutUint32(byteSlice[0:4], message_size)
-	binary.BigEndian.PutUint32(byteSlice[4:8], correlation_id)
+// 	binary.BigEndian.PutUint32(byteSlice[0:4], message_size)
+// 	binary.BigEndian.PutUint32(byteSlice[4:8], correlation_id)
 
-	_, err := connection.Write(byteSlice)
+// 	_, err := connection.Write(byteSlice)
 
-	return err
+// 	return err
+// }
+
+func handleKafkaRequest (connection net.Conn){
+	defer connection.Close()
+
+	sizeBuffer := make([]byte, 4)
+	if _, err := io.ReadFull(connection, size); err != nill{
+		return
+	}
+
+	message_size := binary.BigEndian.Uint32(sizeBuffer)
+
+	bodyBuffer := make([] byte, message_size)
+	if _, err := io.ReadFull(connection, bodyBuffer); err != nil{
+		return
+	}
+
+	correlation_id := binary.BigEndian.Uint32(bodyBuffer[4:8])
+	fmt.Printf("Received Correlation ID: %d\n", correlationID)
+
+	response := make([] byte, 8)
+	binary.BigEndian.PutUint32(response[0:4], message_size)
+	binary.BigEndian.PutUint32(response[4:8], correlation_id)
+
+	connection.Write(response)
 }
 
 
@@ -34,31 +59,42 @@ func sendResponse(connection net.Conn) error {
 
 
 
-
-
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
 
-	// TODO: Uncomment the code below to pass the first stage
+	l, _ := net.Listen("tcp", "0.0.0.0:9092")
+	for{
+		connection, _ := l.Accept()
+		go handleKafkaRequest(connection)
+	}
+
+
+
+
+
+
+
+	// // You can use print statements as follows for debugging, they'll be visible when running tests.
+	// fmt.Println("Logs from your program will appear here!")
+
+	// // TODO: Uncomment the code below to pass the first stage
 	
-	l, err := net.Listen("tcp", "0.0.0.0:9092")
-	if err != nil {
-		fmt.Println("Failed to bind to port 9092")
-		os.Exit(1)
-	}
+	// l, err := net.Listen("tcp", "0.0.0.0:9092")
+	// if err != nil {
+	// 	fmt.Println("Failed to bind to port 9092")
+	// 	os.Exit(1)
+	// }
 
-	for {
-		connection, err := l.Accept()
-		if err != nil {
-			fmt.Println("Error accepting connection: ", err.Error())
-			continue
-		}
+	// for {
+	// 	connection, err := l.Accept()
+	// 	if err != nil {
+	// 		fmt.Println("Error accepting connection: ", err.Error())
+	// 		continue
+	// 	}
 
-		err = sendResponse(connection)
-		if err != nil{
-			fmt.Println("Error sending Kafka response", err.Error)
-		}
-		connection.Close()
-	}
+	// 	err = sendResponse(connection)
+	// 	if err != nil{
+	// 		fmt.Println("Error sending Kafka response", err.Error)
+	// 	}
+	// 	connection.Close()
+	// }
 }
