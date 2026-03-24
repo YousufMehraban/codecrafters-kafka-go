917387d90311d308ebdd2df52a542ca9befb3912
Mohammad Yousuf Mehraban <yousufmehraban@gmail.com>
2026-03-23 17:07:24 -0700

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -58,6 +58,8 @@ func handleKafkaRequest (connection net.Conn){
 	if apiVersion < 0 || apiVersion > 4 {
 		sendError(connection, correlationID, errorApiUnsupportedVersion)
 		return
+	} else {
+		sendError(connection, correlationID, errorNone)
 	}
 
 }
