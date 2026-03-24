a5a20843b181b8f08c2d8976bc7845e76cc33455
Mohammad Yousuf Mehraban <yousufmehraban@gmail.com>
2026-03-23 20:44:19 -0700

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -31,51 +31,84 @@ var _ = os.Exit
 // const errorNone int16 = 0
 // const errorApiUnsupportedVersion = 35
 
-const(
-	errorNone int16 = 0
-	errorApiUnsupportedVersion int16 = 35
-)
+// const(
+// 	errorNone int16 = 0
+// 	errorApiUnsupportedVersion int16 = 35
+// )
 
-func handleKafkaRequest (connection net.Conn){
-	defer connection.Close()
+// func handleKafkaRequest (connection net.Conn){
+// 	defer connection.Close()
 
-	sizeBuffer := make([]byte, 4)
-	if _, err := io.ReadFull(connection, sizeBuffer); err != nil{
-		return
-	}
+// 	sizeBuffer := make([]byte, 4)
+// 	if _, err := io.ReadFull(connection, sizeBuffer); err != nil{
+// 		return
+// 	}
 
-	messageSize := binary.BigEndian.Uint32(sizeBuffer)
+// 	messageSize := binary.BigEndian.Uint32(sizeBuffer)
 
-	bodyBuffer := make([] byte, messageSize)
-	if _, err := io.ReadFull(connection, bodyBuffer); err != nil{
-		return
-	}
-	apiKey := int16(binary.BigEndian.Uint16(bodyBuffer[0:2]))
-	apiVersion := int16(binary.BigEndian.Uint16(bodyBuffer[2:4]))
-	correlationID := binary.BigEndian.Uint32(bodyBuffer[4:8])
-	fmt.Printf("Received Correlation ID: %d, apiKey: %d\n", correlationID, apiKey)
+// 	bodyBuffer := make([] byte, messageSize)
+// 	if _, err := io.ReadFull(connection, bodyBuffer); err != nil{
+// 		return
+// 	}
+// 	apiKey := int16(binary.BigEndian.Uint16(bodyBuffer[0:2]))
+// 	apiVersion := int16(binary.BigEndian.Uint16(bodyBuffer[2:4]))
+// 	correlationID := binary.BigEndian.Uint32(bodyBuffer[4:8])
+// 	fmt.Printf("Received Correlation ID: %d, apiKey: %d\n", correlationID, apiKey)
 
-	if apiVersion < 0 || apiVersion > 4 {
-		sendError(connection, correlationID, errorApiUnsupportedVersion)
-		return
-	} else {
-		sendError(connection, correlationID, errorNone)
-	}
+// 	if apiVersion < 0 || apiVersion > 4 {
+// 		sendError(connection, correlationID, errorApiUnsupportedVersion)
+// 		return
+// 	} else {
+// 		sendError(connection, correlationID, errorNone)
+// 	}
 
+// }
+
+// func sendError(connection net.Conn, correlationID uint32, errorCode int16){
+// 	response := make([] byte, 10)
+// 	binary.BigEndian.PutUint32(response[0:4], 6)
+// 	binary.BigEndian.PutUint32(response[4:8], correlationID)
+// 	binary.BigEndian.PutUint16(response[8:10], uint16(errorCode))
+
+// 	connection.Write(response)
+// }
+
+
+
+func handleKafkaRequest(connection net.Conn, correlationID uint32){
+
+	body := [] byte {}
+	body = append(body, 0, 0)
+	body = append(body, 2)
+
+	apiKeyBuffer := make([] byte, 6)
+
+	binary.BigEndian.PutUint16(apiKeyBuffer[0:2], 18) // api key
+	binary.BigEndian.PutUint16(apiKeyBuffer[2:4], 0)  // min api version
+	binary.BigEndian.PutUint16(apiKeyBuffer[4:6], 4)  // max api version
+
+	body = append(body, apiKeyBuffer...)    // api key compact array
+	body = append(body, 0) 					// tagged fields
+
+	throttleBuffer := make([] byte, 4) 		// creating a throttle time compact array usually is 0
+	throttleBuffer = append(throttleBuffer, 0)
+
+	body = append(body, throttleBuffer...)		// adding throttle time compact array data into body
+	body = append(body, 0)					// main tagged fields
+
+	// final body packet 
+	// 4 bytes messageSize + 4 bytes correlationID + len(body)
+
+	totalSize := 4 + len(body)				// storing total length of body plus corralationID.
+
+	response := make([] byte, 4 + totalSize)		// creating response slice containing messageSize plus totalSize
+
+	binary.BigEndian.PutUint32(response[0:4], uint32(totalSize))   // setting first 4 bytes of response as messageSize
+	binary.BigEndian.PutUint32(response[4:8], correlationID)		// setting second 4 bytes of response as corralationID
+	copy(response[8:], body)									// setting from 8th bytes to the end as body of response
+
+	connection.Write(response)								// writing/sending response throught the connection
 }
-
-func sendError(connection net.Conn, correlationID uint32, errorCode int16){
-	response := make([] byte, 10)
-	binary.BigEndian.PutUint32(response[0:4], 6)
-	binary.BigEndian.PutUint32(response[4:8], correlationID)
-	binary.BigEndian.PutUint16(response[8:10], uint16(errorCode))
-
-	connection.Write(response)
-}
-
-
-
-
 
 
 
