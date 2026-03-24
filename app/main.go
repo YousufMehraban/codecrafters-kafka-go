59fe0faec9fcc373a4d5e267cd3718a876f611f7
Mohammad Yousuf Mehraban <yousufmehraban@gmail.com>
2026-03-23 21:15:57 -0700

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -135,8 +135,9 @@ func main() {
 	l, err := net.Listen("tcp", "0.0.0.0:9092")
 	if err != nil {
 		fmt.Println("Failed to bind to port 9092")
-		os.Exit(1)
+		return
 	}
+	dffer l.Close()
 
 	for {
 		connection, err := l.Accept()
